9d76e3f4ec10c6a5e5baedb0adf5d60213e1bb8e
unknown <lucasdafonsecadepaula@gmail.com>
2021-02-23 20:28:03 -0300

    material ui

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+
 
 export default function Home() {
   return (
     <div>
       comecando
+      <Button color="primary">Hello World</Button>;
     </div>
   );
 }
