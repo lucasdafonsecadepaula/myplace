dea2fe95a55f97af254ec85e078ffc4ffe372aee
unknown <lucasdafonsecadepaula@gmail.com>
2021-02-26 19:33:05 -0300

    Com SearchBar

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Layout from '../src/components/Layout';
 import Map from '../src/components/Map';
 import Box from '@material-ui/core/Box';
+import SearchBar from '../src/components/SearchBar';
 
 
 
 
 export default function Home() {
+    
   return (
     <>
         <Box component="div" overflow="hidden">
             <Layout title="MyPlace"></Layout>
             <Map></Map>
+            <SearchBar />
         </Box>
     </>
   );
